aeb9bc6a706168d8f04e05f557495480395bd091
Triyas Septiyanto <101114925+ThreeYas@users.noreply.github.com>
2022-12-02 20:51:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,36 +24,61 @@ st.write("Triyas Septiyanto | 200411100043 | Penambangan Data A")
 ket_data , dataset, preprocessing, modeling, implementasi = st.tabs(["Keterangan Data","Dataset", "Preprocessing", "Modeling", "Implementasi"])
 
 with ket_data:
+    
+    st.write("## Penjelasan Dataset")
     st.write("""
-            * Kode_Kontrak :
+            Data ini digunakan untuk Studi Kasus Peminjaman Uang
+
+            Credit risk adalah resiko yang harus ditanggung oleh seorang individu atau lembaga ketika memberikan pinjaman - biasanya dalam bentuk uang - ke individu atau pihak lain. Resiko ini berupa tidak bisa dibayarkannya pokok dan bunga pinjaman, sehingga mengakibatkan kerugian berikut: — gangguan aliran kas (cash flow) sehingga modal kerja terganggu. — meningkatkan biaya operasional untuk mengejar pembayaran tersebut (collection). Untuk memperkecil resiko kredit ini, biasanya dilakukan proses yang disebut dengan credit scoring dan credit rating terhadap pihak peminjam. Output proses ini akan menjadi basis untuk menentukan apakah aplikasi pengajuan pinjaman baru diterima atau ditolak.
+
+            Credit score adalah nilai resiko yang diberikan kepada seorang individu atau organisasi yang mengajukan pinjaman berdasarkan rekam jejak pinjaman dan pembayaran yang dilakukan. Proses pemberian credit score ini biasanya disebut sebagai credit scoring. Perhitungan credit score biasanya dibuat berdasarkan data historis lamanya keterlambatan pembayaran dan yang tidak bayar sama sekali (bad debt). Bad debt biasanya mengakibatkan lembaga pemberian kredit harus menyita aset atau melakukan write off. Nilai credit score biasanya bervariasi antar lembaga. Namun banyak yang kemudian mengadopsi model FICO Score yang memiliki rentang nilai 300 - 850. Semakin tinggi nilai yang didapatkan, maka semakin baik tingkat kemampuan seseorang atau sebuah lembaga untuk membayar pinjaman.
+
+            Kadang banyak lembaga yang menggunakan risk rating atau tingkat resiko. Terbalik dengan credit score, semakin tinggi rating ini menunjukkan resiko yang semakin meningkat. Selain itu kodifikasi juga dibuat lebih simpel dibandingkan rentang nilai sehingga keputusan yang bisa diambil lebih cepat. Contoh, misalkan penggunaan kombinasi seperti huruf AAA, AA+, P-1, dan seterusnya. Atau untuk banyak internal lembaga peminjam, kategorisasi hanya menggunakan rentang angka yang kecil misalkan 1 sampai dengan 5.
+            """)
+
+    st.write("## Sumber Dataset")
+    st.write("Dataset Credit Score Dari Kaggle berbentuk XLSX")
+    kaggle = "https://www.kaggle.com/code/kerneler/starter-credit-scoring-c358cfde-2/data"
+    st.markdown(f'[Link Dataset Credit Score Kaggle ]({kaggle})')
+    st.write("Dataset Credit Score Dari Github berbentuk CSV")
+    github = "https://raw.githubusercontent.com/ThreeYas/credit-scoring/main/credit-scoring.csv"
+    st.markdown(f'[Link Dataset Credit Score Github ]({github})')
+
+    st.write("## Penjelasan Kolom")
+    st.write("""
+            * Kode Kontrak :
             Kolom kode_kontrak merupakan identitas dari sebuah data. identitas data tidak perlu diikutkan untuk klasifikasi. Sehingga kolom ini akan diabaikan saat klasifikasi data.Pada kolom ini merupakan kolom identitas pada nasabah yang menjadi pelanggan.
             * Pendapatan Setahun (Juta):
             Kolom pendapatan_setahun_juta merupakan data yang bertype numerik. Ciri-ciri data bertype numerik adalah data tersebut bernilai angka dan bertype integer. Pada kolom ini merupakan kolom pendapatan nasabah pada satu tahun dan memiliki satuan juta.
             * KPR Aktif :
             [YA, TIDAK]
+
             Kolom kpr_aktif merupakan data bertype categorial/oridinal sehingga harus di normalisasikan agar berubah menjadi numerik. Ciri-ciri data bertype categorial yaitu data tersebut merupakan nama dari suatu hal, dan ciri data bertype ordinal yaitu memiliki 2 keadaan. Pada kolom ini merupakan kolom kredit pemilik rumah yang dimiliki nasabah. jika nasabah tersebut mempunyai KPR aktif, maka data tersebut bernilai "Ya"
             * Durasi Pinjaman (Bulan) :
             [12 Bulan, 24 Bulan, 36 Bulan, 48 Bulan]
+
             Kolom durasi_pinjaman_bulan data bertype numerik. Ciri-ciri data bertype numerik adalah data tersebut bernilai angka dan bertype integer. Pada kolom ini merupakan kolom durasi pinjaman yang akan diajukan oleh nasabah dalam satuan bulan.
             * Jumlah Tanggungan :
             [0 - 6 Tanggungan]
+
             Kolom jumlah_tanggungan merupakan data bertype numerik. Ciri-ciri data bertype numerik adalah data tersebut bernilai angka dan bertype integer. Pada kolom ini merupakan jumlah tanggungan yang dimiliki nasaban saat dirumah.
             * Rentan Hari (rata_rata_overdue): 
             [0 - 30 hari, 31 - 45 hari, 46 - 60 hari, 61 - 90 hari, > 90 hari]
+
             Kolom rata_rata_overdue merupakan data bertype categorial sehingga harus di normalisasikan agar berubah menjadi numerik. Ciri-ciri data bertype categorial yaitu data tersebut merupakan nama dari suatu hal, Pada kolom ini merupakan kolom jangka pengembalian yang dipinjam oleh nasabah dalam waktu rentang yang sudah disediakan.
             * Nilai Resiko (risk_rating):
             [1 - 5]
+
             Kolom risk_rating merupakan class dengan type data numerik. Ciri-ciri data bertype numerik adalah data tersebut bernilai angka dan bertype integer. Kolom ini merupakan kelas dari data. dan yang akan di prediksi nanti merupakan data yang ada pada kolom ini.
             """)
+    
+    st.write("## Repository Github")
+    st.write(" Kode ini dapat di akses di Github ThreeYas")
+    repo = "https://github.com/ThreeYas/Aplikasi-Data-Mining"
+    st.markdown(f'[ Link Repository Github ]({repo})')
 
 with dataset:
-    st.write("Dataset Credit Score Dari Kaggle berbentuk XLSX")
-    kaggle = "https://www.kaggle.com/code/kerneler/starter-credit-scoring-c358cfde-2/data"
-    st.markdown(f'[Dataset Credit Score Kaggle ]({kaggle})')
-    st.write("Dataset Credit Score Dari Github berbentuk CSV")
-    github = "https://raw.githubusercontent.com/ThreeYas/credit-scoring/main/credit-scoring.csv"
-    st.markdown(f'[Dataset Credit Score Github ]({github})')
-
+    st.write("Dataset ini terdiri dari 900 baris dan 8 kolom")
     # membaca dataset csv dari url
     url = 'https://raw.githubusercontent.com/ThreeYas/credit-scoring/main/credit-scoring.csv'
     data = pd.read_csv(url)
@@ -61,12 +86,30 @@ with dataset:
 
 with preprocessing:
     st.write(" # Preprocessing ")
-    data.head()
+    st.write("Data yang belum dinormalisasi")
+    st.dataframe(data)
+    
+    # memilih fitur kelas
+    label = data["risk_rating"]
+
+    st.write("Perintah DROP COLUMN digunakan untuk menghapus kolom di tabel yang sudah ada.")
+    st.write("Menghapus kolom risk_rating dari tabel")
     # menghapus kolom risk_rating dari tabel
     X = data.drop(columns=["risk_rating"])
 
     # periksa apakah variabel target telah dihapus
-    X.head()
+    st.dataframe(X)
+
+    st.write("""
+            Mengubah kolom *rata_rata_overdue* menjadi tipe data numerik
+            
+            Memisahkan kolom numerik berdasarkan *rentan hari*
+
+            ```
+            # Rentan Hari
+            [0 - 30 hari, 31 - 45 hari, 46 - 60 hari, 61 - 90 hari, > 90 hari]
+            ```
+            """)
 
     # memecah setiap kelas pada kolom "rata_rata_overdue" menjadi kolom tersendiri
     split_kolom_overdue = pd.get_dummies(X["rata_rata_overdue"], prefix="overdue")
@@ -75,17 +118,12 @@ with preprocessing:
     # menghapus kolom rata_rata_overdue dari tabel
     X = X.drop(columns = "rata_rata_overdue")
 
-    # memilih fitur kelas
-    label = data["risk_rating"]
-
     # memecah setiap kelas pada kolom "kpr_aktif" menjadi kolom tersendiri
     split_kolom_kpr = pd.get_dummies(X["kpr_aktif"], prefix="KPR")
     X = X.join(split_kolom_kpr)
 
     # menghapus kolom kpr_aktif dari tabel
     X = X.drop(columns = "kpr_aktif")
-
-    st.write("Data preprocessing adalah proses yang mengubah data mentah ke dalam bentuk yang lebih mudah dipahami. Proses ini penting dilakukan karena data mentah sering kali tidak memiliki format yang teratur.")
 
     st.write("menampilkan dataframe dimana rata-rata overdue, risk rating dan kpr aktif sudah di hapus dari data")
     st.dataframe(X)
